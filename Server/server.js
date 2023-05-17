6e0cd9abfbaa17aa932243d3a736b0cead783715
pavan <pks10802@gmail.com>
2023-05-18 05:11:01 +0530

    Update-2

--- Server/server.js
+++ Server/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 
 //server
 const app = express() 
-const PORT = process.env.PORT || 6000
+const PORT = process.env.PORT || 5000
 const router = express.Router()
 dotenv.config()
 
